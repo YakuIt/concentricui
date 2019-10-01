5da41d2f173ca8a9c781405847a17353c78c5d42
Yakult <eddcoventry@gmail.com>
2019-10-01 23:06:48 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,11 @@ from setuptools import setup
 
 setup(
     name='concentricui',
-    version='0.1',
-    packages=['concentricui'],
-    url='https://github.com/YakuIt/concentricui',
+    version='0.1.1',
+    packages=['concentricui', 'concentricui.circle', 'concentricui.oblong', 'concentricui.widgets',
+              'concentricui.behaviours', 'concentricui.appcontroll', 'concentricui.colourscheme',
+              'concentricui.roundedrectangle'],
+    url='https://github.com/YakuIt/concentricui/',
     license='',
     author='edd',
     author_email='',
