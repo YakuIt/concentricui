ca1a414908b64aa0c4103eeccbdeee8e815ce720
edd <eddcoventry@gmail.com>
2019-10-04 18:17:07 +0100

    started adding plyer-like methods

--- setup.py
+++ setup.py
@@ -3,7 +3,8 @@ from setuptools import setup
 setup(
     name='concentricui',
     version='0.1.1',
-    packages=['concentricui', 'concentricui.circle', 'concentricui.oblong', 'concentricui.widgets',
+    packages=['concentricui', 'concentricui.pseudoplyer', 'concentricui.circle', 'concentricui.oblong',
+              'concentricui.widgets',
               'concentricui.behaviours', 'concentricui.appcontroll', 'concentricui.colourscheme',
               'concentricui.roundedrectangle'],
     url='https://github.com/YakuIt/concentricui/',
