605eaed67265576cdfbb305c3478dcbe7a280e8b
edd <eddcoventry@gmail.com>
2019-10-04 18:17:51 +0100

    started adding plyer-like methods

---
+++ concentricui/pseudoplyer/keepscreenon.py
@@ -0,0 +1,33 @@
+from kivy.utils import platform
+
+if platform is 'android':
+    from jnius import autoclass
+
+    PythonActivity = autoclass('org.kivy.android.PythonActivity')
+    View = autoclass('android.view.View')
+    Params = autoclass('android.view.WindowManager$LayoutParams')
+    from android.runnable import run_on_ui_thread
+
+
+class ScreenFlagSetter(object):
+
+    def __init__(self):
+        pass
+
+    def set_screen_on_flag(self, *args):
+        if platform is 'android':
+            self.android_setflag()
+
+    def clear_screen_on_flag(self, *args):
+        if platform is 'android':
+            self.android_clearflag()
+
+    if platform is 'android':
+        @run_on_ui_thread
+        def android_setflag(self):
+            PythonActivity.mActivity.getWindow().addFlags(Params.FLAG_KEEP_SCREEN_ON)
+
+    if platform is 'android':
+        @run_on_ui_thread
+        def android_clearflag(self):
+            PythonActivity.mActivity.getWindow().clearFlags(Params.FLAG_KEEP_SCREEN_ON)
