fe4303a0132281579624f5764917e72895a41eb4
edd <eddcoventry@gmail.com>
2019-10-18 15:52:28 +0100

    method invert_state added for toggle buttons

--- concentricui/behaviours/concentrictogglebutton.py
+++ concentricui/behaviours/concentrictogglebutton.py
@@ -11,3 +11,6 @@ class ConcentricToggleButton(ConcentricButton, ToggleButtonBehavior):
 
     def __init__(self, **kwargs):
         super(ConcentricButton, self).__init__(**kwargs)
+
+    def invert_state(self):
+        self.state = 'down' if self.state == 'normal' else 'normal'
