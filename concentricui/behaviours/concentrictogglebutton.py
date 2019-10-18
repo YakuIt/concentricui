3d348758d5ea6928e4a4058c49aa916ac82ca2a8
edd <eddcoventry@gmail.com>
2019-10-18 16:18:29 +0100

    added *args to parameters for previous commit's methods

--- concentricui/behaviours/concentrictogglebutton.py
+++ concentricui/behaviours/concentrictogglebutton.py
@@ -12,11 +12,11 @@ class ConcentricToggleButton(ConcentricButton, ToggleButtonBehavior):
     def __init__(self, **kwargs):
         super(ConcentricButton, self).__init__(**kwargs)
 
-    def invert_state(self):
+    def invert_state(self, *args):
         self.state = 'down' if self.state == 'normal' else 'normal'
 
-    def set_state_down(self):
+    def set_state_down(self, *args):
         self.state = 'down'
 
-    def set_state_normal(self):
+    def set_state_normal(self, *args):
         self.state = 'normal'
